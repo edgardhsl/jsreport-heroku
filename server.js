6bef2b276369ae3462734c0d40519c9dad29c08d
Edgard Hufelande <edgardhwp@outlook.com>
2021-03-14 17:48:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const jsreport = require('jsreport')()
+const jsreport = require('jsreport')({
+	httpPort: 3000
+})
 
 if (process.env.JSREPORT_CLI) {
   // export jsreport instance to make it possible to use jsreport-cli
